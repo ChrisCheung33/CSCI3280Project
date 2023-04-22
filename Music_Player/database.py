d7978a90db7ad57d1df1bd84326d6db40c2faf60
SKTT1Flash <hoho.cc1@gmail.com>
2023-04-22 14:42:45 +0800

    Display unknown song info

--- Music_Player/database.py
+++ Music_Player/database.py
@@ -18,10 +18,10 @@ def add_music(filename, title, album, length, artist, lyrics=None, album_art=Non
         title = os.path.splitext(title)[0]
 
     if album == None or album == "":
-        album = "None"
+        album = "Unknown"
     
     if artist == None or artist == "":
-        artist = "None"
+        artist = "Unknown"
 
     if not music_df[music_df['title'] == title].empty:
         print(f"A song with the title '{title}' already exists in the database.")
@@ -56,10 +56,10 @@ def update_music(filename, title, album, artist):
         title = os.path.splitext(title)[0]
 
     if album == None or album == "":
-        album = "None"
+        album = "Unknown"
     
     if artist == None or artist == "":
-        artist = "None"
+        artist = "Unknown"
 
     music_info_row = {'filename': filename,
                         'album': album,
